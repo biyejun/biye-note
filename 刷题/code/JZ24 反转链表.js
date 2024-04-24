b8d025c49982a224bbc04b7f00d506d09d505564
biyejun <bnbiye@163.com>
2024-04-24 18:14:03 +0800

    docs: jZ24 反转链表

--- 刷题/code/JZ24 反转链表.js
+++ 刷题/code/JZ24 反转链表.js
@@ -11,19 +11,19 @@ n1.next = n2
 n2.next = n3
 
 /* 
-反转链表
+反转链表 递归
 */
 function ReverseList(head) {
-	let pre = null
-	let cur = head
-
-	while (cur) {
-		let tempNext = cur.next
-		cur.next = pre
-		pre = cur
-		cur = tempNext
+	if (head === null || head.next === null) {
+		return head
 	}
-	return pre
+
+	const newHead = ReverseList(head.next) // 递归反转子链表，newHead是反转后的子链表头节点
+
+	head.next.next = head // 将当前节点的下一个节点指向前一个节点，实现反转
+	head.next = null // 将当前节点的next置为null，防止循环引用
+
+	return newHead // 返回反转后的链表头节点
 }
 
 const newHead = ReverseList(n1)
