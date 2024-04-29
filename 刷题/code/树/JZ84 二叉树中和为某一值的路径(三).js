5bb5a6130a5538bab6991c0cb0e8d75360293eb0
biyejun <bnbiye@163.com>
2024-04-29 14:41:22 +0800

    docs: jZ84 二叉树中和为某一值的路径
    哈希表 一次dfs

--- 刷题/code/树/JZ84 二叉树中和为某一值的路径(三).js
+++ 刷题/code/树/JZ84 二叉树中和为某一值的路径(三).js
@@ -4,40 +4,58 @@ function TreeNode(x) {
 	this.right = null
 }
 
-/* 使用一个对象来封装结果，以便在递归调用中传递和修改 */
-let res = {
-	value: 0,
-}
-
-/* 
-深度优先搜索 根左右
-*/
-function dfs(root, sum, res) {
+function dfs(root, sum, last, mp) {
+	//last为到上一层为止的累加和
 	if (!root) {
-		return
+		//空结点直接返回
+		return 0
 	}
 
-	if (root.val === sum) {
-		res.value++ // 符合目标值，增加结果计数
+	let res = 0
+	let temp = root.val + last //到目前结点为止的累加和
+
+	if (mp.hasOwnProperty(temp - sum)) {
+		//如果该累加和减去sum在哈希表中出现过，相当于减去前面的分支
+		res += mp[temp - sum] //加上有的路径数
 	}
 
-	dfs(root.left, sum - root.val, res)
-	dfs(root.right, sum - root.val, res)
+	if (!mp.hasOwnProperty(temp)) {
+		mp[temp] = 0
+	}
+	mp[temp]++ //增加该次路径和
+
+	res += dfs(root.left, sum, temp, mp) //进入子结点
+	res += dfs(root.right, sum, temp, mp)
+
+	mp[temp]-- //回退该路径和，因为别的树枝不需要这边存的路径和
+
+	if (mp[temp] === 0) {
+		delete mp[temp]
+	}
+
+	return res
 }
 
 /* 
-两次dfs
-第一次： 遍历二叉树的每个结点
-第二次： 遍历以每个结点为根的子树
+一次dfs + 哈希表
+
+https://blog.nowcoder.net/n/9f3db96916704e1cae1b7ff4956d1bd2?f=comment
+
 */
 function FindPath(root, sum) {
-	if (!root) {
-		return res.value // 如果根为空，则返回当前的结果计数
-	}
-
-	dfs(root, sum, res) // 查询某节点为根的路径数
-	FindPath(root.left, sum, res) // 继续查询其他节点为根的路径数
-	FindPath(root.right, sum, res)
-
-	return res.value
+	let mp = {} //记录路径和及条数
+	mp[0] = 1 //路径和为0的有1条
+	return dfs(root, sum, 0, mp)
 }
+
+const root = new TreeNode(1)
+const n2 = new TreeNode(2)
+const n3 = new TreeNode(3)
+const n0 = new TreeNode(0)
+
+root.left = n2
+n2.right = n0
+root.right = n3
+
+const result = FindPath(root, 3)
+console.log(result, 'result')
