fdd8a5291539d6217eb723be0508248ae70c5dca
biyejun <bnbiye@163.com>
2024-04-29 15:53:20 +0800

    docs: jZ86 在二叉树中找到两个节点的最近公共祖先

--- 刷题/code/树/JZ86 在二叉树中找到两个节点的最近公共祖先.js
+++ 刷题/code/树/JZ86 在二叉树中找到两个节点的最近公共祖先.js
@@ -4,50 +4,33 @@ function TreeNode(x) {
 	this.right = null
 }
 
+function dfs(root, o1, o2) {
+	if (!root || root.val === o1 || root.val === o2) {
+		return root
+	}
+
+	let left = dfs(root.left, o1, o2)
+	let right = dfs(root.right, o1, o2)
+
+	//如果left为空，说明这两个节点在root结点的右子树上，我们只需要返回右子树查找的结果即可
+	if (!left) {
+		return right
+	}
+	// 同上
+	if (!right) {
+		return left
+	}
+
+	//如果left和right都不为空，说明这两个节点一个在root的左子树上一个在root的右子树上，
+	//我们只需要返回cur结点即可。
+	return root
+}
+
 /* 
-层次遍历
+递归
+
+https://blog.nowcoder.net/n/43706e8eb6c6486183d5016b9eb78dc9?f=comment
 */
 function lowestCommonAncestor(root, o1, o2) {
-	// 使用一个对象来模拟HashMap记录每个节点的父节点
-	const parent = {}
-	const queue = []
-
-	// 根节点没有父节点 给一个特殊测值
-	parent[root.val] = null
-	queue.push(root)
-
-	// 层次遍历树 记录每个父节点 直到找到 o1和o2
-	while (queue.length > 0 && !(parent.hasOwnProperty(o1) && parent.hasOwnProperty(o2))) {
-		const currentNode = queue.shift()
-
-		if (currentNode.left) {
-			parent[currentNode.left.val] = currentNode.val
-			queue.push(currentNode.left)
-		}
-
-		if (currentNode.right) {
-			parent[currentNode.right.val] = currentNode.val
-			queue.push(currentNode.right)
-		}
-	}
-
-	// 如果没有找到o1 o2，则返回null
-	if (!(parent.hasOwnProperty(o1) && parent.hasOwnProperty(o2))) {
-		return null
-	}
-
-	// 使用一个集合来记录o1及其所有祖先节点
-	const ancestors = new Set()
-	let currentNodeVal = o1
-	while (currentNodeVal !== null) {
-		ancestors.add(currentNodeVal)
-		currentNodeVal = parent[currentNodeVal]
-	}
-
-	currentNodeVal = o2
-	while (!ancestors.has(currentNodeVal)) {
-		currentNodeVal = parent[currentNodeVal]
-	}
-
-	return currentNodeVal
+	return dfs(root, o1, o2).val
 }
