ca26c021a36c97f8e002a29d8068d9a9d7b910b0
biyejun <bnbiye@163.com>
2024-04-30 14:21:34 +0800

    docs: jZ53 数字在升序数组中出现的次数

--- 刷题/code/搜索算法/JZ53 数字在升序数组中出现的次数.js
+++ 刷题/code/搜索算法/JZ53 数字在升序数组中出现的次数.js
@@ -1,12 +1,31 @@
-/* 暴力求解 */
-function GetNumberOfK(nums, k) {
-	let count = 0
+// [1, 2, 3, 3, 3, 3, 4, 5]
 
-	for (let i = 0; i < nums.length; i++) {
-		if (nums[i] === k) {
-			count++
+function branrySearch(array, k) {
+	let l = 0
+	let r = array.length
+
+	while (l < r) {
+		let m = l + Math.floor((r - l) / 2)
+		if (array[m] >= k) {
+			r = m
+		} else {
+			l = m + 1
 		}
 	}
 
-	return count
+	return l
 }
+
+/* 二分查找 */
+function GetNumberOfK(nums, k) {
+	// 找到左边界
+	const first = branrySearch(nums, k)
+	// 找到右边界
+	const last = branrySearch(nums, k + 1)
+	// 若超出数组范围，或第一个找到的索引对应的值不是k，则证明目标值出现的次数为0
+	// 否则，右边界减去左边界即能统计出目标出现的次数
+	return first === nums.length || nums[first] !== k ? 0 : last - first
+}
+
+const arr = [1, 2, 3, 3, 3, 3, 4, 5]
+GetNumberOfK(arr, -1)
