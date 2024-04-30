57a7573ccf8c2155cecd71d12496e15a99ff0afc
biyejun <bnbiye@163.com>
2024-04-30 11:03:02 +0800

    docs: jZ59 滑动窗口的最大值

--- 刷题/code/队列&栈/JZ59 滑动窗口的最大值.js
+++ 刷题/code/队列&栈/JZ59 滑动窗口的最大值.js
@@ -1,16 +1,35 @@
 /* 
-暴力解法
-遍历所有的滑动窗口 比较窗口中的最大值 保存
+双端队列
 */
 function maxInWindows(num, size) {
-	const res = []
-	if (num.length === 0 || size < 1 || num.length < size) {
-		return res
+	const ret = []
+
+	if (num.length === 0 || size < 1 || num.length < size) return ret
+
+	const dq = [] // 使用数组来模拟双端队列
+
+	for (let i = 0; i < num.length; i++) {
+		// 移除队列尾部所有小于当前元素的索引
+		while (dq.length > 0 && num[dq[dq.length - 1]] < num[i]) {
+			dq.pop()
+		}
+
+		dq.push(i) // 将当前索引加入队列尾部
+
+		// 判断队列的头部索引是否过期
+		if (dq[0] + size <= i) {
+			dq.shift() // 移除队列头部过期的索引
+		}
+
+		// 判断是否形成了窗口
+		if (i + 1 >= size) {
+			ret.push(num[dq[0]]) // 将当前窗口的最大值加入结果数组
+		}
 	}
 
-	for (let i = 0; i < num.length - size + 1; i++) {
-		res.push(Math.max(...num.slice(i, i + size)))
-	}
-
-	return res
+	return ret
 }
+
+const arr = [2, 3, 4, 2, 6, 2, 5, 1]
+const res = maxInWindows(arr, 3)
+console.log(res)
